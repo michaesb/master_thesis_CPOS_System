cef793c5ea45e958f8530f8a9fc682260337464a
Michael Sund|en Bitney <michaesb@rfe.uio.no>
2020-08-26 10:53:56 +0200

    added noise function in extra

--- extra/error_calculation_NMA_standard.py
+++ extra/error_calculation_NMA_standard.py
@@ -2,5 +2,18 @@ import numpy as np
 
 
 
-def accuracy(Z,t):
-    
+def accuracy_NMEA(x):
+  """
+  Calculates the noise over a time period t.
+  Assumes that the length of the vector is 60
+  """
+  length= 60
+  if not len(z)==length:
+     raise ValueError("the length of input variable is not 60")
+  sigma = 1./(N-1)
+  
+  sigma = sigma*np.sum( abs(x - np.sum(x)/length)**2 )
+  
+  sigma = sigma**0.5 #root of sigma
+
+  return sigma

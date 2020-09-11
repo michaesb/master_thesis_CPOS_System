6c95512ca3a96eae5239ce72a11ccd816377ab51
Michael <michaesb>
2020-09-11 12:52:56 +0200

    added a correction the filtering requirements

--- extra/error_calculation_NMA_standard.py
+++ extra/error_calculation_NMA_standard.py
@@ -4,6 +4,23 @@ import numpy as np
 the accuracy of a sample mean
 """
 
+
+def filtering_outliers(z):
+    N = len(z)
+    interval = 60
+    z_copy = z.copy()
+    for i in range(0,len(z),interval):
+        ave_z = np.mean(z_copy[i:i+interval])
+        z_60 = abs(z_copy[i:i+interval]-ave_z)
+        z_temp = np.where( z_60 > 0.2,np.nan,z_copy[i:i+interval])
+        z_temp = np.where( z_60 > 3*np.std(z_copy), np.nan,z_temp)
+        z_copy[i:i+interval] = z_temp
+    z_resized = z_copy[np.logical_not(np.isnan(z_copy))]
+
+    print(N,len(z_resized), "size of array", "percentage=", 100*len(z_resized)/N)
+    return z, z_resized
+
+
 def accuracy_NMEA(z):
     """
     Calculates the noise over a time period t.
--- laptop/noise_smooth.py
+++ laptop/noise_smooth.py
@@ -4,27 +4,10 @@ from scipy.signal import savgol_filter
 import sys, time
 sys.path.insert(0, "..")
 from extra.progressbar import progress_bar
-from extra.error_calculation_NMA_standard import accuracy_NMEA
+from extra.error_calculation_NMA_standard import accuracy_NMEA, filtering_outliers
 from data_reader_NMEA.NMEA_data_reader import ReadNMEAData
 
-
-def filtering_outliers(z):
-    N = len(z)
-    interval = 60
-    z_copy = z.copy()
-    for i in range(0,len(z),interval):
-        ave_z = np.mean(z_copy[i:i+interval])
-        z_60 = abs(z_copy[i:i+interval]-ave_z)
-        z_temp = np.where( z_60 > 0.2,np.nan,z_copy[i:i+interval])
-        z_temp = np.where( z_60 > 3*np.std(z_60), np.nan,z_temp)
-        z_copy[i:i+interval] = z_temp
-    z_resized = z_copy[np.logical_not(np.isnan(z_copy))]
-
-    print(N,len(z_resized), "size of array", "percentage=", 100*len(z_resized)/N)
-    return z, z_resized
-
 def plotting_coordinates():
-
     ave_N, ave_E, ave_Z = np.mean(N),np.mean(E),np.mean(Z)
     t_filtered = np.linspace(0,24,len(Z_filtered))
 

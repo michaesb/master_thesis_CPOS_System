fixed the noise calculation, but still need to fix time axis

extra/error_calculation_NMA_standard.py
@@ -8,12 +8,11 @@ def filtering_outliers(z,verbose=False):
     N = len(z)
     interval = 60
     z_copy = z.copy()
-    for i in range(0,len(z),interval):
-        ave_z = np.mean(z_copy[i:i+interval])
-        z_60 = abs(z_copy[i:i+interval]-ave_z)
-        z_temp = np.where(z_60 > 0.2,np.nan,z_copy[i:i+interval])
-        z_temp = np.where(z_60 > 3*np.std(z_copy), np.nan,z_temp)
-        z_copy[i:i+interval] = z_temp
+    ave_z = np.median(z_copy)
+    z_60 = abs(z_copy-ave_z)
+    z_temp = np.where(z_60 > 0.2,np.nan,z_copy)
+    z_temp = np.where(z_60 > 3*np.std(z_copy),np.nan,z_temp)
+    z_copy = z_temp
     z_resized = z_copy[np.logical_not(np.isnan(z_copy))]
     if verbose:
         print(N,len(z_resized), "size of array", "percentage=", 100*len(z_resized)/N)
@@ -27,6 +26,6 @@ def accuracy_NMEA(z):
     N = len(z); size = int(N-60)
     sigma = np.zeros(size)
     for i in range(30,N-30):
-        z_ave = np.sum(z[(i-30):(i+30)])/size
-        sigma[i-30] = ((np.sum(abs(z[i-30:(i+30)]) - z_ave)**2 )/((60)-1))**0.5
+        z_ave = np.sum(z[(i-30):(i+30)])/60
+        sigma[i-30] = (((np.sum( abs(z[i-30:(i+30)]-z_ave) )**2 )/((60)-1)))**0.5
     return sigma

seasonal_data/single_day_plot.py
@@ -1,16 +1,13 @@
 import numpy as np
-from scipy.signal import savgol_filter
 import matplotlib.pyplot as plt
-
+from scipy.signal import savgol_filter
 import sys, time
 sys.path.insert(0, "..")
 from extra.progressbar import progress_bar
 from extra.error_calculation_NMA_standard import accuracy_NMEA, filtering_outliers
 from data_reader_NMEA.NMEA_data_reader import ReadNMEAData
 
-
 def plotting_coordinates():
-    ave_N, ave_E, ave_Z = np.mean(N),np.mean(E),np.mean(Z)
     t_filtered = np.linspace(0,24,len(Z_filtered))
 
     plt.subplot(3,1,1)
@@ -20,7 +17,7 @@ def plotting_coordinates():
     plt.xticks([])
 
     plt.subplot(3,1,2)
-    plt.plot(obj.time_h, Z-ave_Z, "r*", label="unfiltered")
+    plt.plot(obj.time_h, Z-np.mean(Z), "r*", label="unfiltered")
     plt.plot(t_filtered, Z_filtered-np.mean(Z_filtered), "b*",label="filtered")
     plt.ylabel("coordinate Z [m]")
     plt.legend()
@@ -35,31 +32,26 @@ def plotting_coordinates():
     plt.show()
 
 
-receiver_stations = [ "NAK","RAN", "HFS", "SIM","STA", "STE", "TRM"]
-date = "045"
+receiver_stations = ["HFS", "NAK","RAN", "SIM","STA", "STE", "TRM"]
+date = "151"
 year = "2018"
+
 for receiver in receiver_stations:
+    
+    adress = "/scratch/michaesb/data/NMEA/"+year+"/"+date+"/"+"NMEA_M"+receiver +"_"+date+"0.log"
+    obj = ReadNMEAData()
     try:
-       # adress_M = "/run/media/michaelsb/HDD Linux/data/NMEA/"+year+"/"+date+"/NMEA_M"\
-       # +receiver+"_"+date+"0.log"
-         adress_M = "/scratch/michaesb/data/NMEA/"+year+"/"+date+"/NMEA_M"\
-         +receiver+"_"+date+"0.log"
-        obj = ReadNMEAData()
-        obj.read_textfile(adress_M,verbose=False,filter_4=True)
-    except FileNotFoundError:
-         adress_M = "/scratch/michaesb/data/NMEA/"+year+"/"+date+"/NMEA_M"\
-         +receiver+"_"+date+"0.log"
-         obj = ReadNMEAData()
-         obj.read_textfile(adress_M,verbose=False,filter_4=True)
+        obj.read_textfile(adress,verbose=False,filter_4=True)
     except:
-        print("no"+receiver+"file her")
+        print("path not found")
+        print(adress)
         continue
     obj.display_GPS_indicator()
     print(obj.day_year, "day_year")
     N, E, Z = obj.coordinates
-    ave_N, ave_E, ave_Z = np.mean(N),np.mean(E),np.mean(Z)
-    print(obj.datapoints[0]/obj.datapoints[1]," % datapoints")
-    Z,Z_filtered = filtering_outliers(Z)
+    print(obj.datapoints[0]/obj.datapoints[1],"percentage datapoints gps fix")
+    Z,Z_filtered = filtering_outliers(Z,verbose=True)
     sigma_Z = accuracy_NMEA(Z_filtered-np.mean(Z_filtered))
     sigma_Z_smooth= savgol_filter(sigma_Z,window_length=(5*60+1),polyorder=3)
     plotting_coordinates()
+
2c9339765fcbf08d178317e56376027548eaba5d
Michael S. Bitney <michaesb>
2020-10-21 20:27:28 +0200

    adding usage from home compyter

--- seasonal_data/supermag_data.py
+++ seasonal_data/supermag_data.py
@@ -2,14 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys, time
 sys.path.insert(0, "../") # to get access to adjecent packages in the repository
-
 from supermag_substorm_reader.substorm_event_reader import ReadSubstormEvent
 
-
-path = "/scratch/michaesb/substorm_event_list_2018.csv"
 obj = ReadSubstormEvent()
-obj.read_csv(path)
-obj.
+
+
+try:
+    path = "/scratch/michaesb/substorm_event_list_2018.csv"
+    obj.read_csv(path)
+
+except FileNotFoundError:
+    path = "/run/media/michaelsb/HDD Linux/data/substorm_event_list_2018.csv"
+    obj.read_csv(path)
+    
 def filtering_to_Norway_night(X):
     X_filtered = X
     return X_filtered

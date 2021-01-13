7f674f9654472a47ccbdad5c98e3194696d8c657
Michael <>
2021-01-13 20:04:51 +0100

    adding changes to send to computer. gps event collection not functional yet

--- magnetometer_event/bins_noise_gps.py
+++ magnetometer_event/bins_noise_gps.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import sys, time
+from numba import njit, prange
 from collections import Counter
-sys.path.insert(0, "../") # to get access to adjecent packages in the repository
+
+sys.path.insert(0, "../")  # to get access to adjecent packages in the repository
 from extra.time_date_conversion import date_to_days
 from magnetometer_event.filtering_events import filtering_to_Norway_night
 from supermag_substorm_reader.magnetometer_reader import ReadMagnetomerData
@@ -14,137 +16,199 @@ from noise_gps_function import run_NMEA_data
 
 
 def plot_histograms(bins_sorted, time_day_bins, time_of_event):
-    borders = [bins_sorted[int((len(bins_sorted)-1)/3)],\
-               bins_sorted[int((len(bins_sorted)-1)*2/3)]]
+    borders = [
+        bins_sorted[int((len(bins_sorted) - 1) / 3)],
+        bins_sorted[int((len(bins_sorted) - 1) * 2 / 3)],
+    ]
 
-    index_third, index_two_thirds = int(len(bins_sorted)/3),\
-                                    int(len(bins_sorted)*2/3)
+    index_third, index_two_thirds = int(len(bins_sorted) / 3), int(
+        len(bins_sorted) * 2 / 3
+    )
 
-    plt.hist(bins_sorted, bins = 30)
-    plt.axvline(x=borders[0], color ="r")
-    plt.axvline(x=borders[1], color ="r")
-    plt.title("2018, "+station+",\n Max magnetometer value of a substorm event. \n"+\
-              "Red lines are the lines for the borders which is " +str(borders[0])\
-              +" and " +str(borders[1])+ " nT")
+    plt.hist(bins_sorted, bins=30)
+    plt.axvline(x=borders[0], color="r")
+    plt.axvline(x=borders[1], color="r")
+    plt.title(
+        "2018, "
+        + station
+        + ",\n Max magnetometer value of a substorm event. \n"
+        + "Red lines are the lines for the borders which is "
+        + str(borders[0])
+        + " and "
+        + str(borders[1])
+        + " nT"
+    )
     plt.xlabel("minimum of the north component magnetometer [nT]")
     plt.ylabel("number of occurances")
     plt.show()
-    plt.hist(time_day_bins, bins = 30)
+    plt.hist(time_day_bins, bins=30)
     plt.title("Time of year when the substorm occurs")
     plt.xlabel("day of year")
     plt.ylabel("number of occurances")
     plt.show()
-    plotting_bins_array = np.concatenate([time_of_event[time_of_event>16]-20,time_of_event[time_of_event<16]+4])
-    plt.hist(plotting_bins_array,color = "b", bins = 80)
+    plotting_bins_array = np.concatenate(
+        [time_of_event[time_of_event > 16] - 20, time_of_event[time_of_event < 16] + 4]
+    )
+    plt.hist(plotting_bins_array, color="b", bins=80)
     plt.title("Distrubution of what time the substorm occurs")
     plt.xlabel("time of day [UT+1]")
-    plt.xticks(range(9),labels=[str(20),str(21),str(22),str(23),str(24),\
-                                str(1),str(2),str(3),str(4)])
+    plt.xticks(
+        range(9),
+        labels=[
+            str(20),
+            str(21),
+            str(22),
+            str(23),
+            str(24),
+            str(1),
+            str(2),
+            str(3),
+            str(4),
+        ],
+    )
     plt.show()
 
-def plot_all_mag_events(events_collection,bins_sorted):
-    borders = [bins_sorted[int((len(bins_sorted)-1)/3)],bins_sorted[int((len(bins_sorted)-1)*2/3)]]
 
-    index_third, index_two_thirds = int(len(events_collection)/3),\
-                                    int(len(events_collection)*2/3)
+def plot_all_mag_events(events_collection, bins_sorted):
+    borders = [
+        bins_sorted[int((len(bins_sorted) - 1) / 3)],
+        bins_sorted[int((len(bins_sorted) - 1) * 2 / 3)],
+    ]
+
+    index_third, index_two_thirds = int(len(events_collection) / 3), int(
+        len(events_collection) * 2 / 3
+    )
     hour_area = 3
-    time = np.linspace(-(hour_area/2 -1),(hour_area/2+1),7)*60
+    time = np.linspace(-(hour_area / 2 - 1), (hour_area / 2 + 1), 7) * 60
     for i in range(len(events_collection)):
-        plt.plot(events_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_collection, axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("All recorded substorms by the magnetometer in "+station+" in 2018")
+        plt.plot(events_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(events_collection, axis=0)
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title("All recorded substorms by the magnetometer in " + station + " in 2018")
     plt.xlabel("minutes")
     plt.ylabel("North component B-value [nT]")
-    plt.xticks(np.linspace(0,len(events_collection)-60-hour_area*10,7),time)
+    plt.xticks(np.linspace(0, len(events_collection) - 60 - hour_area * 10, 7), time)
     plt.legend()
     plt.show()
 
     for i in range(index_two_thirds, len(events_collection)):
-        plt.plot(events_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_collection[index_two_thirds:], axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("Third bin of substorms by the magnetometer in "+station+" in 2018")
+        plt.plot(events_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(events_collection[index_two_thirds:], axis=0)
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title("Third bin of substorms by the magnetometer in " + station + " in 2018")
     plt.xlabel("minutes")
-    plt.ylabel("North component B-value [nT] (smaller than "+str(borders[1])+" nT)")
-    plt.xticks(np.linspace(0,len(events_collection)-60-hour_area*10,7),time)
+    plt.ylabel("North component B-value [nT] (smaller than " + str(borders[1]) + " nT)")
+    plt.xticks(np.linspace(0, len(events_collection) - 60 - hour_area * 10, 7), time)
     plt.legend()
     plt.show()
 
-    for i in range(index_third,index_two_thirds):
-        plt.plot(events_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_collection[index_third:index_two_thirds], axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("Second bin of substorms by the magnetometer in "+station+" in 2018")
+    for i in range(index_third, index_two_thirds):
+        plt.plot(events_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(
+        events_collection[index_third:index_two_thirds], axis=0
+    )
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title("Second bin of substorms by the magnetometer in " + station + " in 2018")
     plt.xlabel("minutes")
-    plt.ylabel("North component B-value [nT] (between "+str(borders[0])+"nT and "+str(borders[1])+" nT)")
-    plt.xticks(np.linspace(0,len(events_collection)-60-hour_area*10,7),time)
+    plt.ylabel(
+        "North component B-value [nT] (between "
+        + str(borders[0])
+        + "nT and "
+        + str(borders[1])
+        + " nT)"
+    )
+    plt.xticks(np.linspace(0, len(events_collection) - 60 - hour_area * 10, 7), time)
     plt.legend()
     plt.show()
 
     for i in range(index_third):
-        plt.plot(events_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_collection[:index_third], axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("First bin of recorded substorms by the magnetometer in "+station+" in 2018")
-    plt.ylabel("North component B-value [nT] (bigger than "+str(borders[0])+" nT)")
+        plt.plot(events_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(events_collection[:index_third], axis=0)
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title(
+        "First bin of recorded substorms by the magnetometer in " + station + " in 2018"
+    )
+    plt.ylabel("North component B-value [nT] (bigger than " + str(borders[0]) + " nT)")
     plt.xlabel("minutes")
-    plt.xticks(np.linspace(0,len(events_collection)-60-hour_area*10,7),time)
+    plt.xticks(np.linspace(0, len(events_collection) - 60 - hour_area * 10, 7), time)
     plt.legend()
     plt.show()
 
 
-def plot_all_gps_events(events_gps_collection,bins_sorted):
-    borders = [bins_sorted[int((len(bins_sorted)-1)/3)],bins_sorted[int((len(bins_sorted)-1)*2/3)]]
+def plot_all_gps_events(events_gps_collection, bins_sorted):
+    borders = [
+        bins_sorted[int((len(bins_sorted) - 1) / 3)],
+        bins_sorted[int((len(bins_sorted) - 1) * 2 / 3)],
+    ]
 
-    index_third, index_two_thirds = int(len(events_gps_collection)/3),\
-                                    int(len(events_gps_collection)*2/3)
+    index_third, index_two_thirds = int(len(events_gps_collection) / 3), int(
+        len(events_gps_collection) * 2 / 3
+    )
     hour_area = 3
-    time = np.linspace(-(hour_area/2 -1),(hour_area/2+1),7)*60
+    time = np.linspace(-(hour_area / 2 - 1), (hour_area / 2 + 1), 7) * 60
     for i in range(len(events_gps_collection)):
-        plt.plot(events_gps_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_gps_collection, axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("All recorded substorms by the magnetometer in "+station+" in 2018")
+        plt.plot(events_gps_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(events_gps_collection, axis=0)
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title("All recorded substorms by the magnetometer in " + station + " in 2018")
     plt.xlabel("minutes")
     plt.ylabel("North component B-value [nT]")
-    plt.xticks(np.linspace(0,len(events_gps_collection)-60-hour_area*10,7),time)
+    plt.xticks(
+        np.linspace(0, len(events_gps_collection) - 60 - hour_area * 10, 7), time
+    )
     plt.legend()
     plt.show()
 
     for i in range(index_two_thirds, len(events_gps_collection)):
-        plt.plot(events_gps_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_gps_collection[index_two_thirds:], axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("Third bin of substorms by the magnetometer in "+station+" in 2018")
+        plt.plot(events_gps_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(events_gps_collection[index_two_thirds:], axis=0)
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title("Third bin of substorms by the magnetometer in " + station + " in 2018")
     plt.xlabel("minutes")
-    plt.ylabel("North component B-value [nT] (smaller than "+str(borders[0])+" nT)")
-    plt.xticks(np.linspace(0,len(events_gps_collection)-60-hour_area*10,7),time)
+    plt.ylabel("North component B-value [nT] (smaller than " + str(borders[0]) + " nT)")
+    plt.xticks(
+        np.linspace(0, len(events_gps_collection) - 60 - hour_area * 10, 7), time
+    )
     plt.legend()
     plt.show()
 
-    for i in range(index_third,index_two_thirds):
-        plt.plot(events_gps_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_gps_collection[index_third:index_two_thirds], axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("Second bin of substorms by the magnetometer in "+station+" in 2018")
+    for i in range(index_third, index_two_thirds):
+        plt.plot(events_gps_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(
+        events_gps_collection[index_third:index_two_thirds], axis=0
+    )
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title("Second bin of substorms by the magnetometer in " + station + " in 2018")
     plt.xlabel("minutes")
-    plt.ylabel("North component B-value [nT] (between "+str(borders[0])+"nT and "+str(borders[1])+" nT)")
-    plt.xticks(np.linspace(0,len(events_gps_collection)-60-hour_area*10,7),time)
+    plt.ylabel(
+        "North component B-value [nT] (between "
+        + str(borders[0])
+        + "nT and "
+        + str(borders[1])
+        + " nT)"
+    )
+    plt.xticks(
+        np.linspace(0, len(events_gps_collection) - 60 - hour_area * 10, 7), time
+    )
     plt.legend()
     plt.show()
 
     for i in range(index_third):
-        plt.plot(events_gps_collection[i,:], linewidth = 0.5)
-    average_event = np.nanmedian(events_gps_collection[:index_third], axis = 0)
-    plt.plot(average_event, linewidth = 3, color = "black", label="median value")
-    plt.title("First bin of recorded substorms by the magnetometer in "+station+" in 2018")
-    plt.ylabel("North component B-value [nT] (substorms above "+str(borders[1])+" nT)")
+        plt.plot(events_gps_collection[i, :], linewidth=0.5)
+    average_event = np.nanmedian(events_gps_collection[:index_third], axis=0)
+    plt.plot(average_event, linewidth=3, color="black", label="median value")
+    plt.title(
+        "First bin of recorded substorms by the magnetometer in " + station + " in 2018"
+    )
+    plt.ylabel(
+        "North component B-value [nT] (substorms above " + str(borders[1]) + " nT)"
+    )
     plt.xlabel("minutes")
-    plt.xticks(np.linspace(0,len(events_gps_collection)-60-hour_area*10,7),time)
+    plt.xticks(
+        np.linspace(0, len(events_gps_collection) - 60 - hour_area * 10, 7), time
+    )
     plt.legend()
     plt.show()
-
 
 
 obj_event = ReadSubstormEvent()
@@ -152,22 +216,23 @@ obj_mag = ReadMagnetomerData()
 
 try:
     laptop_path = "/scratch/michaesb/"
-    path_event = laptop_path+"substorm_event_list_2018.csv"
-    path_mag = laptop_path+"20201025-17-57-supermag.csv"
-    obj_event.read_csv(path_event,verbose = False)
-    print("substorm event reader")
-    obj_mag.read_csv(path_mag, verbose = False)
+    path_event = laptop_path + "substorm_event_list_2018.csv"
+    path_mag = laptop_path + "20201025-17-57-supermag.csv"
+    obj_event.read_csv(path_event, verbose=False)
+    print("substorm done")
     print("magnetometer reader")
+    obj_mag.read_csv(path_mag, verbose=False)
+    print("magnetometer reader done")
 
 except FileNotFoundError:
     desktop_path = "/run/media/michaelsb/HDD Linux/data/"
-    path_event = desktop_path+"/substorm_event_list_2018.csv"
-    path_mag = desktop_path+"/20201025-17-57-supermag.csv"
+    path_event = desktop_path + "/substorm_event_list_2018.csv"
+    path_mag = desktop_path + "/20201025-17-57-supermag.csv"
     print("substorm event reader")
-    obj_event.read_csv(path_event,verbose = False)
+    obj_event.read_csv(path_event, verbose=False)
     print("substorm done")
     print("magnetometer reader")
-    obj_mag.read_csv(path_mag, verbose = False)
+    obj_mag.read_csv(path_mag, verbose=False)
     print("magnetometer done")
 
 
@@ -176,17 +241,34 @@ try:
     station = sys.argv[1]
 except IndexError:
     station = "TRO"
-dates_mag, time_UTC_mag,\
-location_long,location_lat,\
-geographic_north,geographic_east, geographic_z, \
-magnetic_north,magnetic_east, magnetic_z = obj_mag.receiver_specific_data(station)
-stations_dictionary_GEO_coord = {"KIL":[69.02, 20.79], "TRM":[69.66, 18.94],\
-                                 "ABK":[68.35, 18.82], "AND":[69.30, 16.03],\
-                                 "DOB":[62.07,  9.11], "DON":[66.11, 12.50],\
-                                 "JCK":[66.40, 16.98], "KAR":[59.21,  5.24],\
-                                 "MAS":[69.46, 23.70], "NOR":[71.09, 25.79],\
-                                 "RVK":[64.94, 10.99], "SOL":[61.08,  4.84],\
-                                 "SOR":[70.54, 22.22]}
+(
+    dates_mag,
+    time_UTC_mag,
+    location_long,
+    location_lat,
+    geographic_north,
+    geographic_east,
+    geographic_z,
+    magnetic_north,
+    magnetic_east,
+    magnetic_z,
+) = obj_mag.receiver_specific_data(station)
+
+stations_dictionary_GEO_coord = {
+    "KIL": [69.02, 20.79],
+    "TRM": [69.66, 18.94],
+    "ABK": [68.35, 18.82],
+    "AND": [69.30, 16.03],
+    "DOB": [62.07, 9.11],
+    "DON": [66.11, 12.50],
+    "JCK": [66.40, 16.98],
+    "KAR": [59.21, 5.24],
+    "MAS": [69.46, 23.70],
+    "NOR": [71.09, 25.79],
+    "RVK": [64.94, 10.99],
+    "SOL": [61.08, 4.84],
+    "SOR": [70.54, 22.22],
+}
 
 ########################## then event reader  #############################
 lat = obj_event.latitude
@@ -195,22 +277,43 @@ time_UTC_event = obj_event.dates_time
 dates_event, year = obj_event.day_of_year
 
 Norway_time = time_UTC_event + 1
-lat, mag_time, Norway_time, dates_event = filtering_to_Norway_night(lat,mag_time,Norway_time,dates_event)
+lat, mag_time, Norway_time, dates_event = filtering_to_Norway_night(
+    lat, mag_time, Norway_time, dates_event
+)
 
 ########################## then gps noise  ##################################
+
+
 def create_fake_noise():
-    n = 50500
-    time_axis_gps = np.zeros((365,n))*np.nan
+    n = 50200
+    time_axis_gps = np.zeros((365, n + 365)) * np.nan
+    gps_noise = np.zeros((365, n + 365)) * np.nan
     for i in range(365):
-        time_axis_gps[i,:] = np.linspace(0,24,n)
-    gps_noise = np.random.random((365,n))
-    return time_axis_gps,gps_noise
+        time_axis_gps[i, : n + i] = np.linspace(0, 24, n + i)
+        gps_noise[i, : n + i] = np.random.random(n + i)
+    return time_axis_gps, gps_noise
+
 
 # time_axis_gps,gps_noise =run_NMEA_data(365,"TRM")
-time_axis_gps,gps_noise =create_fake_noise()
+t1 = time.time()
+time_axis_gps, gps_noise = create_fake_noise()
+print("time", time.time() - t1)
 ########################## creating bins ###################################
-bins_sorted,time_day_bins, time_of_event,events_collection_sorted,noise_gps_sorted = \
-create_bins_with_noise_sort(dates_mag,dates_event, Norway_time,time_UTC_mag ,magnetic_north, gps_noise, time_axis_gps)
+(
+    bins_sorted,
+    time_day_bins,
+    time_of_event,
+    events_collection_sorted,
+    noise_gps_sorted,
+) = create_bins_with_noise_sort(
+    dates_mag,
+    dates_event,
+    Norway_time,
+    time_UTC_mag,
+    magnetic_north,
+    gps_noise,
+    time_axis_gps,
+)
 
 ###########################plotting different data ##########################
 
@@ -218,4 +321,4 @@ create_bins_with_noise_sort(dates_mag,dates_event, Norway_time,time_UTC_mag ,mag
 
 # plot_all_mag_events(events_collection_sorted,bins_sorted)
 
-plot_all_gps_events(noise_gps_sorted,bins_sorted)
+plot_all_gps_events(noise_gps_sorted, bins_sorted)
--- magnetometer_event/creating_bins.py
+++ magnetometer_event/creating_bins.py
@@ -4,8 +4,7 @@ from collections import Counter
 sys.path.insert(0, "../") # to get access to adjecent packages in the repository
 from extra.time_date_conversion import date_to_days
 
-def create_bins(dates_mag,dates_event, time_of_event, \
-                time_UTC_mag, magnetometer_values):
+def create_bins(dates_mag,dates_event, time_of_event, time_UTC_mag, magnetometer_values):
     N_mag = len(dates_mag)
     N_event = len(dates_event)
     days_event = date_to_days(dates_event)
@@ -55,7 +54,7 @@ def create_bins_with_noise_sort(dates_mag,dates_event, time_of_event, \
     time_day_bins = np.zeros(N_event)*np.nan
     hour_area = 3
     events_collection = np.zeros((N_event,int(hour_area*60)))*np.nan
-    event_gps_collection = np.zeros((N_event, int(hour_area*3600)))*np.nan
+    event_gps_collection = np.zeros((N_event, int(50500*hour_area/24)))*np.nan
     ii_bins=0
     iii_gps =0
     date = 0
@@ -64,13 +63,15 @@ def create_bins_with_noise_sort(dates_mag,dates_event, time_of_event, \
             if date != days_magnetometer[i]:
                 date = days_magnetometer[i]
                 for k in range(Counter(days_event)[days_magnetometer[i]]):
+                    N_gps_points = len(time_gps[int(days_magnetometer[i]),:])
+                    N_gps_points_next = len(time_gps[int(days_magnetometer[i])+1,:])
                     index_min, index_max = i+int((time_of_event[ii_bins] - (hour_area/2))*60)\
                                           ,i+int((time_of_event[ii_bins] + (hour_area/2))*60)
-                    gps_min_time =np.round(time_of_event[ii_bins] -(hour_area/2),2)
-                    gps_max_time =np.round(time_of_event[ii_bins] +(hour_area/2),2)
+
+                    gps_min_time = time_of_event[ii_bins] -(hour_area/2)
+                    gps_max_time = time_of_event[ii_bins] +(hour_area/2)
                     if index_max-index_min != hour_area*60:
                         index_min+=index_max-index_min-hour_area*60
-
                     bin_value = np.min(magnetometer_values[index_min:index_max])
                     if bin_value != bins[ii_bins-1]:
                         bins[ii_bins] = bin_value
@@ -78,19 +79,34 @@ def create_bins_with_noise_sort(dates_mag,dates_event, time_of_event, \
                         events_collection[ii_bins,:] = magnetometer_values[index_min:index_max]
                         #gps
                         print(gps_min_time,gps_max_time)
-                        print(gps_min_time,time_gps.shape)
-
-                        index_min_gps = np.where(gps_min_time ==np.round(time_gps[int(days_magnetometer[i]),:],3))
-                        print(time_gps, np.sum(gps_min_time ==np.round(time_gps[int(days_magnetometer[i]),:],3)))
-                        print("index_min_gps",index_min_gps, gps_min_time>24,)
-
+                        index_min_gps = np.where(gps_min_time < np.round(time_gps[int(days_magnetometer[i]),:],2))
+                        index_min_gps = int(index_min_gps[0][0])
+                        print("index_min, total number of points, index_point output, correct target \n"\
+                        ,index_min_gps,"   ",N_gps_points, time_gps[int(days_magnetometer[i]),index_min_gps], gps_min_time)
                         if gps_max_time>24 and gps_min_time <24:
-                            print(days_magnetometer[i], index_min_gps)
-                            index_max_gps = np.where(gps_max_time-24 ==time_gps[int(days_magnetometer[i])+1,:])
-                            event_gps_collection[ii_bins,:(24-gps_min_time)*3600] = gps_noise[int(int(days_magnetometer[i])),index_min_gps:]
-                            event_gps_collection[ii_bins,(24-gps_min_time)*3600:] = gps_noise[int(int(days_magnetometer[i+1])),:index_max_gps]
+                            index_max_gps = np.where(gps_max_time-24>np.round(time_gps[int(days_magnetometer[i])+1,:],2))
+                            index_max_gps = int(index_max_gps[0][-1])
+
+                            index_min_limit = len(gps_noise[int(days_magnetometer[i]),index_min_gps:])
+                            index_max_limit = len(gps_noise[int(days_magnetometer[i])+1,:index_max_gps])+index_min_limit
+
+
+                            print("index_max_gps, total number of points, index_point output, correct target \n"\
+                            ,index_max_gps,"   ",N_gps_points, time_gps[int(days_magnetometer[i]+1),index_max_gps], gps_max_time-24)
+
+                            #filling the event_gps_collection with the first array
+                            event_gps_collection[ii_bins,0:index_min_limit] = gps_noise[int(days_magnetometer[i]),index_min_gps:]
+                            #filling the event_gps_collection with the second array
+                            print("limits",index_min_limit, index_max_limit-index_min_limit, index_max_limit)
+                            print(len(event_gps_collection[ii_bins,index_min_limit:index_max_limit]), index_max_limit -index_min_limit)
+                            event_gps_collection[ii_bins,index_min_limit:index_max_limit] =\
+                            gps_noise[int(days_magnetometer[i])+1,0:index_max_gps]
                         else:
-                            index_max_gps = np.where(gps_max_time ==time_gps[int(days_magnetometer[i]),:])
+                            index_max_gps = np.where(gps_max_time > np.round(time_gps[int(days_magnetometer[i]),:],2))
+                            index_max_gps = int(index_max_gps[0][-1])
+                            print("hello there")
+                            print(gps_min_time,gps_max_time,gps_max_time-gps_min_time, "time")
+                            print(index_min_gps,index_max_gps,index_max_gps-index_min_gps, "index")
                             event_gps_collection[ii_bins,:] = gps_noise[int(days_magnetometer[i]),index_min_gps:index_max_gps]
                     ii_bins+=1
     events_collection = events_collection[np.logical_not(np.isnan(bins)),:]

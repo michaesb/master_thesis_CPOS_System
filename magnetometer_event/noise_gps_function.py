1c7a62b8e0ddaa4f71e580df1b568f0d49b81ee8
michaesb <Michaelsb7@gmail.com>
2021-04-12 16:01:12 +0200

    checking for transformation errors, and think I found some

--- magnetometer_event/noise_gps_function.py
+++ magnetometer_event/noise_gps_function.py
@@ -98,7 +98,7 @@ def run_NMEA_data(nr_days, receiver):
         time_axis = np.zeros((nr_days,nr_datapoints))*np.nan
         year = "2018"
         date = create_date(1,nr_days)
-        for i in tqdm(range(len(date)),desc= "RTIM data"):
+        for i in tqdm(range(len(date)),desc= "NMEA data"):
             adress = "/run/media/michaelsb/data_ssd/data/NMEA/"+year+"/"+date[i]+"/"+\
             "NMEA_M"+receiver +"_"+date[i]+"0.log"
             obj = ReadNMEAData()
@@ -128,5 +128,61 @@ def run_NMEA_data(nr_days, receiver):
             noise[i,:len(noise_temp)]  = noise_temp
             time_axis[i,:len(obj.time_4)] = obj.time_4
         return time_axis,noise
+
+def compare_altitude_transformation(nr_days,receiver):
+    nr_datapoints = 50400
+    altitude_array = np.zeros((nr_days,nr_datapoints))*np.nan
+    altitude_transformed = np.zeros((nr_days,nr_datapoints))*np.nan
+    year = "2018"
+    date = create_date(1,nr_days)
+    for i in tqdm(range(len(date)),desc= "NMEA data"):
+        adress = "/run/media/michaelsb/data_ssd/data/NMEA/"+year+"/"+date[i]+"/"+\
+        "NMEA_M"+receiver +"_"+date[i]+"0.log"
+        obj = ReadNMEAData()
+        try:
+            obj.read_textfile(adress,verbose=False)
+            N,E,Z = obj.coordinates
+            altitude = obj.altitude_only
+            home_computer = 1
+        except FileNotFoundError:
+            try:
+                adress = "/scratch/michaesb/data/NMEA/"+year+"/"+date[i]+"/NMEA_M"+ \
+                receiver+"_"+date[i]+"0.log"
+                obj = ReadNMEAData()
+                obj.read_textfile(adress,verbose=False)
+                N,E,Z = obj.coordinates
+                altitude = obj.altitude_only
+
+                Office_computer = 1
+            except FileNotFoundError:
+                tqdm.write("no "+receiver+" file here at day: " + str(i) +" year: "+year)
+                tqdm.write(adress)
+                noise[i,:] = np.nan
+                continue
+        if len(Z) < 60:
+
+            continue
+        # altitude_array[i,:len(altitude)] = altitude
+        # altitude_transformed[i,:len(obj.Z)] = Z
+        plt.figure(0)
+        # plt.plot(Z-np.nanmedian(Z), color="black" ,label="altidude transformed")
+        plt.plot(Z-np.nanmedian(Z), "r.", linewidth = 0.01, alpha=0.1,label="altidude transformed")
+
+        # plt.plot(altitude-np.nanmedian(altitude),color="blue",label="altitude unaltered")
+        plt.plot(altitude-np.nanmedian(altitude), "g.", linewidth = 0.01, alpha=0.1,label="altitude unaltered")
+
+        plt.title("January 1 looking at the effect of transforming the coordinate system")
+        plt.xlabel("nr_of_datapoints")
+        plt.ylabel("height [m]")
+        plt.legend()
+        plt.figure(1)
+        plt.title("January 1 Here is the altitude unaltered minus \n by the transformed altitude. \n So this is the error betwen the altitudes")
+        plt.plot(altitude-np.nanmedian(altitude)-Z+np.nanmedian(Z), "g.", linewidth = 0.01, alpha=0.1,)
+        plt.xlabel("nr_of_datapoints")
+        plt.ylabel("height [m]")
+        plt.show()
+    return time_axis,noise
+
 if __name__ == '__main__':
-    t,gps =run_NMEA_data(100,"TRM")
+    compare_altitude_transformation(15,"TRM")
+    # t,gps =run_NMEA_data(100,"TRM")
